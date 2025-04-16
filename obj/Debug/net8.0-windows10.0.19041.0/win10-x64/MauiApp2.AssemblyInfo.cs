f514bb04ece316f6e4524207952e2f08cead6851
Vaka <kvg_2009@list.ru>
2025-04-16 20:11:04 +0700

    Добавили вариант 1

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+90c4c618422ab10fcdc3b10218f6df48f9c85a3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9c255cd4e67239ed39357ef17d8ac89c0ff8b2da")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
