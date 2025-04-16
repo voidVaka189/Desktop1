9c255cd4e67239ed39357ef17d8ac89c0ff8b2da
Vaka <kvg_2009@list.ru>
2025-04-16 19:31:57 +0700

    Добавили новую страницу ОГ

--- obj/Debug/net8.0-ios/iossimulator-x64/MauiApp2.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/MauiApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+46f2285cc6013deda3ca903410f691ad34cfb0a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+90c4c618422ab10fcdc3b10218f6df48f9c85a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp2.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b1921c7038628cba8a9b518b0864da4ba68bf94f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+90c4c618422ab10fcdc3b10218f6df48f9c85a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
